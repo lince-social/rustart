3d5d6f2f5977076527bd03c05ec5eb9c662b3e4f
Eduardo de Melo Xavier <xaviduds@gmail.com>
2025-09-28 16:18:36 -0300

    super idol de xaorun

--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,69 @@
-mod application;
-mod domain;
-mod infrastructure;
-mod macros;
-mod presentation;
+// mod application;
+// mod domain;
+// mod infrastructure;
+// mod macros;
+// mod presentation;
 
-use axum::{Router, routing::get};
-use std::{env, io::Error, sync::Arc, time::Duration};
+// use axum::{Router, routing::get};
+// use std::{env, io::Error, sync::Arc, time::Duration};
 
-#[tokio::main]
-async fn main() -> Result<(), Error> {
-    let db = connection().await.inspect_err(|e| {
-        log(LogEntry::Error(e.kind(), e.to_string()));
-    })?;
-    let db = Arc::new(db);
+// #[tokio::main]
+fn main() {
+    // let db = connection().await.inspect_err(|e| {
+    //     log(LogEntry::Error(e.kind(), e.to_string()));
+    // })?;
+    // let db = Arc::new(db);
 
-    schema(db.clone()).await.inspect_err(|e| {
-        log(LogEntry::Error(e.kind(), e.to_string()));
-    })?;
+    // schema(db.clone()).await.inspect_err(|e| {
+    //     log(LogEntry::Error(e.kind(), e.to_string()));
+    // })?;
 
-    let services = dependency_injection(db.clone());
+    // let services = dependency_injection(db.clone());
 
-    let move_services = services.clone();
-    tokio::spawn({
-        async move {
-            let services = move_services.clone();
-            loop {
-                println!("Delivering Karma...");
-                let vec_karma = futures::executor::block_on(async {
-                    services.providers.karma.get(KarmaFilters::default()).await
-                });
+    // let move_services = services.clone();
+    // tokio::spawn({
+    //     async move {
+    //         let services = move_services.clone();
+    //         loop {
+    //             println!("Delivering Karma...");
+    //             let vec_karma = futures::executor::block_on(async {
+    //                 services.providers.karma.get(KarmaFilters::default()).await
+    //             });
 
-                if let Err(e) = vec_karma {
-                    log(LogEntry::Error(e.kind(), e.to_string()));
-                } else if let Err(e) =
-                    use_case_karma_deliver(services.clone(), vec_karma.unwrap()).await
-                {
-                    log(LogEntry::Error(e.kind(), e.to_string()));
-                }
+    //             if let Err(e) = vec_karma {
+    //                 log(LogEntry::Error(e.kind(), e.to_string()));
+    //             } else if let Err(e) =
+    //                 use_case_karma_deliver(services.clone(), vec_karma.unwrap()).await
+    //             {
+    //                 log(LogEntry::Error(e.kind(), e.to_string()));
+    //             }
 
-                println!("Karma Delivered!");
-                tokio::time::sleep(Duration::from_secs(60)).await;
-            }
-        }
-    });
+    //             println!("Karma Delivered!");
+    //             tokio::time::sleep(Duration::from_secs(60)).await;
+    //         }
+    //     }
+    // });
 
-    match env::args().nth(1).as_deref() {
-        Some("migrate") => execute_migration(db.clone()).await.inspect_err(|e| {
-            log(LogEntry::Error(e.kind(), e.to_string()));
-        }),
-        _ => {
-            let app = Router::new()
-                .route("/preto_no_branco.ico", get(handler_section_favicon))
-                .merge(section_router(services.clone()))
-                .nest("/collection", collection_router(services.clone()))
-                .nest("/view", view_router(services.clone()))
-                .nest("/table", table_router(services.clone()))
-                .nest("/operation", operation_router(services.clone()));
+    // match env::args().nth(1).as_deref() {
+    //     Some("migrate") => execute_migration(db.clone()).await.inspect_err(|e| {
+    //         log(LogEntry::Error(e.kind(), e.to_string()));
+    //     }),
+    //     _ => {
+    //         let app = Router::new()
+    //             .route("/preto_no_branco.ico", get(handler_section_favicon))
+    //             .merge(section_router(services.clone()))
+    //             .nest("/collection", collection_router(services.clone()))
+    //             .nest("/view", view_router(services.clone()))
+    //             .nest("/table", table_router(services.clone()))
+    //             .nest("/operation", operation_router(services.clone()));
 
-            let listener = tokio::net::TcpListener::bind("0.0.0.0:6174").await.unwrap();
-            println!("Listening on: {}", listener.local_addr().unwrap());
-            axum::serve(listener, app).await?;
-            Ok(())
-        }
-    }?;
+    //         let listener = tokio::net::TcpListener::bind("0.0.0.0:6174").await.unwrap();
+    //         println!("Listening on: {}", listener.local_addr().unwrap());
+    //         axum::serve(listener, app).await?;
+    //         Ok(())
+    //     }
+    // }?;
 
-    Ok(())
+    // Ok(())
+    println!("RustArt")
 }
